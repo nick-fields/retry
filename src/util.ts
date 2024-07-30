e821ace6f64979a914aca2563ba14b3a5bcb0ce7
Juraj Simon <simon@shoptet.cz>
2024-07-30 15:01:16 +0200

    feat: update some messages with utf8 icons for better visibility

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,4 @@
-import { debug } from '@actions/core';
+import { debug, info } from '@actions/core';
 import ms from 'milliseconds';
 import { Inputs } from './inputs';
 
@@ -8,7 +8,7 @@ export async function wait(ms: number) {
 
 export async function retryWait(inputs: Inputs) {
   if (inputs.retry_wait_strategy === 'random') {
-    debug('Using random wait strategy');
+    info('Using random wait strategy');
     const waitSecondsMin = ms.seconds(inputs.retry_wait_seconds_min);
     const waitSecondsMax = ms.seconds(inputs.retry_wait_seconds_max);
     await retryWaitConstant(
@@ -20,13 +20,14 @@ export async function retryWait(inputs: Inputs) {
     return;
   }
 
-  debug('Using fixed wait strategy');
+  info('Using fixed wait strategy');
   const retryWaitSeconds = ms.seconds(inputs.retry_wait_seconds);
   await retryWaitConstant(retryWaitSeconds);
   debug(`Configured wait: ${inputs.retry_wait_seconds}s`);
 }
 
 export async function retryWaitConstant(retryWaitMs: number) {
+  info(`Waiting ${retryWaitMs}ms before retrying`);
   const waitStart = Date.now();
   await wait(retryWaitMs);
   debug(`Waited ${Date.now() - waitStart}ms`);
